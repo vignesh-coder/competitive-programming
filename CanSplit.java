b888a42869d243172166271a5ea99afbe87b9295
Vignesh <31616369+vignesh-coder@users.noreply.github.com>
2019-02-16 00:18:41 +0530

    Update CanSplit.java

--- CanSplit.java
+++ CanSplit.java
@@ -2,25 +2,23 @@
 import java.util.*;
 import java.io.*;
 
-
 class Solution {
 
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        
+
         long n = in.nextLong();
-        
+
         int a = in.nextInt();
-        
+
         int b = in.nextInt();
-        
-        if((n/2)%(a+b) == 0 || (a == b && n%a == 0)){
-        
+
+        if (n % 2 == 0 && ((n / 2) % (a + b) == 0 || (a == b && n % a == 0))) {
+
             System.out.println("YES");
-        }
-        else{
-        
+        } else {
+
             System.out.println("NO");
         }
     }
